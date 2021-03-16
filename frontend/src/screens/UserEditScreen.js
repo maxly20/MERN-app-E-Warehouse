7621ce21e7e8c740343fb3b86196f1d0750b9448
maxly20 <maxly2011@gmail.com>
2021-03-16 12:09:13 +1100

    building user edit screen

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
-import { getUserDetails, updateUser } from '../actions/userActions'
-import { USER_UPDATE_RESET } from '../constants/userConstants'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+import { getUserDetails, updateUser } from '../actions/userActions';
+import { USER_UPDATE_RESET } from '../constants/userConstants';
 
 const UserEditScreen = ({ match, history }) => {
-  const userId = match.params.id
+  const userId = match.params.id;
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const userDetails = useSelector((state) => state.userDetails)
-  const { loading, error, user } = userDetails
+  const userDetails = useSelector(state => state.userDetails);
+  const { loading, error, user } = userDetails;
 
-  const userUpdate = useSelector((state) => state.userUpdate)
+  const userUpdate = useSelector(state => state.userUpdate);
   const {
     loading: loadingUpdate,
     error: errorUpdate,
     success: successUpdate,
-  } = userUpdate
+  } = userUpdate;
 
   useEffect(() => {
     if (successUpdate) {
-      dispatch({ type: USER_UPDATE_RESET })
-      history.push('/admin/userlist')
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push('/admin/userlist');
     } else {
       if (!user.name || user._id !== userId) {
-        dispatch(getUserDetails(userId))
+        dispatch(getUserDetails(userId));
       } else {
-        setName(user.name)
-        setEmail(user.email)
-        setIsAdmin(user.isAdmin)
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
       }
     }
-  }, [dispatch, history, userId, user, successUpdate])
+  }, [dispatch, history, userId, user, successUpdate]);
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(updateUser({ _id: userId, name, email, isAdmin }))
-  }
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }));
+  };
 
   return (
     <>
@@ -68,7 +68,7 @@ const UserEditScreen = ({ match, history }) => {
                 type='name'
                 placeholder='Enter name'
                 value={name}
-                onChange={(e) => setName(e.target.value)}
+                onChange={e => setName(e.target.value)}
               ></Form.Control>
             </Form.Group>
 
@@ -78,7 +78,7 @@ const UserEditScreen = ({ match, history }) => {
                 type='email'
                 placeholder='Enter email'
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={e => setEmail(e.target.value)}
               ></Form.Control>
             </Form.Group>
 
@@ -87,7 +87,7 @@ const UserEditScreen = ({ match, history }) => {
                 type='checkbox'
                 label='Is Admin'
                 checked={isAdmin}
-                onChange={(e) => setIsAdmin(e.target.checked)}
+                onChange={e => setIsAdmin(e.target.checked)}
               ></Form.Check>
             </Form.Group>
 
@@ -98,7 +98,7 @@ const UserEditScreen = ({ match, history }) => {
         )}
       </FormContainer>
     </>
-  )
-}
+  );
+};
 
-export default UserEditScreen
+export default UserEditScreen;
