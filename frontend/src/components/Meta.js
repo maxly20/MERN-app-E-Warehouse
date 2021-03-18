310368b1154a64816f4692d5cc4d46dd50dfddf9
maxly20 <maxly2011@gmail.com>
2021-03-19 10:17:06 +1100

    adding meta

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,7 +12,7 @@ const Meta = ({ title, description, keywords }) => {
 };
 
 Meta.defaultProps = {
-  title: 'Welcome To Max Shop',
+  title: 'Welcome To E-Warehouse',
   description: 'We sell the best products for the most reasonalb price',
   keywords: 'electronics, buy electronics, cheap electroincs',
 };
