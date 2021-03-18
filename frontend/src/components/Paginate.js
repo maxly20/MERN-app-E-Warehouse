23a662a9e28b89e9331129fec5f26328a44582ca
maxly20 <maxly2011@gmail.com>
2021-03-18 11:16:35 +1100

    building pagination

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Pagination } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from 'react';
+import { Pagination } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
   return (
     pages > 1 && (
       <Pagination>
-        {[...Array(pages).keys()].map((x) => (
+        {[...Array(pages).keys()].map(x => (
           <LinkContainer
             key={x + 1}
             to={
@@ -22,7 +22,7 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
         ))}
       </Pagination>
     )
-  )
-}
+  );
+};
 
-export default Paginate
+export default Paginate;
